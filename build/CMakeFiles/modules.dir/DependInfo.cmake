208f097b7d63103997939060a84fc60d4d77874c
Ismail Tapan <ismltpn@gmail.com>
2018-12-20 22:43:29 +0300

    add cmake files

---
+++ build/CMakeFiles/modules.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ismltpn/Belgeler/gtuAutonomous/autonomous-vehicle/build/CMakeFiles/3.10.2/CompilerIdCXX/CMakeCXXCompilerId.cpp" "/home/ismltpn/Belgeler/gtuAutonomous/autonomous-vehicle/build/CMakeFiles/modules.dir/CMakeFiles/3.10.2/CompilerIdCXX/CMakeCXXCompilerId.cpp.o"
+  "/home/ismltpn/Belgeler/gtuAutonomous/autonomous-vehicle/src/modules/Logger/ConsoleLog.cpp" "/home/ismltpn/Belgeler/gtuAutonomous/autonomous-vehicle/build/CMakeFiles/modules.dir/src/modules/Logger/ConsoleLog.cpp.o"
+  "/home/ismltpn/Belgeler/gtuAutonomous/autonomous-vehicle/src/modules/Logger/Syslog.cpp" "/home/ismltpn/Belgeler/gtuAutonomous/autonomous-vehicle/build/CMakeFiles/modules.dir/src/modules/Logger/Syslog.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src/modules/*"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/modules.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/modules.dir/CMakeFiles/3.10.2/CompilerIdCXX/CMakeCXXCompilerId.cpp.o"
+  "CMakeFiles/modules.dir/src/modules/Logger/ConsoleLog.cpp.o"
+  "CMakeFiles/modules.dir/src/modules/Logger/Syslog.cpp.o"
+  "bin/modules.pdb"
+  "bin/modules"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/modules.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
